2e5fa768260f58206fded3028e18b0c71829c69f
Exotic Objects <jim@exoticobjects.com>
2016-03-02 13:43:32 -0500

    .

--- Pods/Target Support Files/Pods-test_sibling_dependencies/Pods-test_sibling_dependencies-frameworks.sh
+++ Pods/Target Support Files/Pods-test_sibling_dependencies/Pods-test_sibling_dependencies-frameworks.sh
@@ -86,12 +86,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-test_sibling_dependencies/SDWebImage.framework"
   install_framework "Pods-test_sibling_dependencies/STHTTPRequest.framework"
-  install_framework "Pods-test_sibling_dependencies/SocketIOClientSwift.framework"
   install_framework "Pods-test_sibling_dependencies/test_sibling_dependencies_pod_a.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-test_sibling_dependencies/SDWebImage.framework"
   install_framework "Pods-test_sibling_dependencies/STHTTPRequest.framework"
-  install_framework "Pods-test_sibling_dependencies/SocketIOClientSwift.framework"
   install_framework "Pods-test_sibling_dependencies/test_sibling_dependencies_pod_a.framework"
 fi
