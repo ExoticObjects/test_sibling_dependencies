b76d635b1de326ef2813b50aa9abe0b058d77848
Exotic Objects <jim@exoticobjects.com>
2016-03-11 11:59:44 -0500

    .

---
+++ Pods/Target Support Files/test_sibling_dependencies_pod_b/test_sibling_dependencies_pod_b-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "PodBHelper.h"
+
+FOUNDATION_EXPORT double test_sibling_dependencies_pod_bVersionNumber;
+FOUNDATION_EXPORT const unsigned char test_sibling_dependencies_pod_bVersionString[];
+
